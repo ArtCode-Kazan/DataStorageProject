bac071d46d4bc9c57a5bff82ca8670a4f4a6d70d
Roostique <mannapovr@sigma-geophys.com>
2022-10-10 17:03:08 +0300

    Add class and instance for inserting deposit data to database

---
+++ dbase-api-server/dbase_api_server/dbase.py
@@ -0,0 +1,33 @@
+import psycopg2
+from psycopg2 import Error
+
+
+class DepositsDatabase:
+    def __init__(self,
+                 user: str,
+                 password: str,
+                 host: str,
+                 port: int, 
+                 database: str
+                 ):
+        self.connection = psycopg2.connect(user=user,
+                                    password=password,
+                                    host=host,
+                                    port=port,
+                                    database=database)
+
+    def insert_deposit_data(self,
+                            area_name: str):
+        """Insert deposit data in 'deposits' table."""
+        cursor = self.connection.cursor()
+        insert_query = '''
+            INSERT INTO deposits(area_name)
+            VALUES(%s);
+        '''
+        cursor.execute(insert_query, area_name)
+
+        try:
+            self.connection.commit()
+            return True
+        except (Exception, Error):
+            return False
