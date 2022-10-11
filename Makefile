a7187942686af45bfa14311191efd4e293894304
Roostique <mannapovr@sigma-geophys.com>
2022-10-11 13:42:21 +0300

    Fixed makefile alias and path to temp folder, renamed dbase.py class method according to review

--- Makefile
+++ Makefile
@@ -1,2 +1,3 @@
+ROOT = /home/sigma-st-6/Temp
 connect-to-db-container:
-	docker run --name sigma_database -p 5433:5432 -e POSTGRES_PASSWORD=password -e PGDATA=/var/lib/postgresql/data/pgdata  -v "/home/sigma-st-6/Temp":/var/lib/postgresql/data db_image
+	docker run --name sigma_database -p 5433:5432 -e POSTGRES_PASSWORD=password -e PGDATA=/var/lib/postgresql/data/pgdata  -v "$(ROOT)":/var/lib/postgresql/data db_image
--- dbase-api-server/dbase_api_server/dbase.py
+++ dbase-api-server/dbase_api_server/dbase.py
@@ -7,22 +7,23 @@ class DepositsDatabase:
                  user: str,
                  password: str,
                  host: str,
-                 port: int, 
+                 port: int,
                  database: str
-                 ):
+                 ) -> None:
         self.connection = psycopg2.connect(user=user,
-                                    password=password,
-                                    host=host,
-                                    port=port,
-                                    database=database)
+                                           password=password,
+                                           host=host,
+                                           port=port,
+                                           database=database)
 
-    def insert_deposit_data(self, area_name: str):
+    def insert_deposit_data(self,
+                            area_name: str) -> bool:
         """Insert deposit data in 'deposits' table."""
         cursor = self.connection.cursor()
-        insert_query = '''
+        insert_query = """
             INSERT INTO deposits(area_name)
             VALUES(%s);
-        '''
+        """
         cursor.execute(insert_query, (area_name,))
 
         try:
