b4f9466fff6a588a175590cf87ec4915950f84fe
Roostique <mannapovr@sigma-geophys.com>
2022-10-11 09:38:06 +0300

    Added class method 'delete' by 'area_name' in 'dbase' file

--- dbase-api-server/dbase_api_server/dbase.py
+++ dbase-api-server/dbase_api_server/dbase.py
@@ -16,15 +16,28 @@ class DepositsDatabase:
                                     port=port,
                                     database=database)
 
-    def insert_deposit_data(self,
-                            area_name: str):
+    def insert_deposit_data(self, area_name: str):
         """Insert deposit data in 'deposits' table."""
         cursor = self.connection.cursor()
         insert_query = '''
             INSERT INTO deposits(area_name)
             VALUES(%s);
         '''
-        cursor.execute(insert_query, area_name)
+        cursor.execute(insert_query, (area_name,))
+
+        try:
+            self.connection.commit()
+            return True
+        except (Exception, Error):
+            return False
+
+    def delete_deposit_data(self, area_name: str):
+        """Delete deposit data from 'deposits' table"""
+        cursor = self.connection.cursor()
+        delete_query = '''
+            DELETE FROM deposits WHERE area_name = %s;
+        '''
+        cursor.execute(delete_query, (area_name,))
 
         try:
             self.connection.commit()
