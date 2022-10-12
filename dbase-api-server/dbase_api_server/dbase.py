b94b27c909fdc6444e060407cdf782ce06bba049
Roostique <mannapovr@sigma-geophys.com>
2022-10-12 16:54:06 +0300

    Fixed exception handling, prints changed to logging, added logging "basicConfig"

--- dbase-api-server/dbase_api_server/dbase.py
+++ dbase-api-server/dbase_api_server/dbase.py
@@ -1,6 +1,13 @@
+import logging
 import psycopg2
 from psycopg2 import connection, OperationalError, DataError
 from psycopg2 import InternalError, ProgrammingError, DatabaseError
+
+
+logging.basicConfig(
+    filename='dbase.log',
+    filemode='w'
+)
 
 
 class DepositsDatabase:
@@ -27,15 +34,15 @@ class DepositsDatabase:
             self.__connection.commit()
             return True
         except OperationalError as error:
-            print(f'{error}: problems with database operation')
+            logging.error(error, 'problems with database operation')
         except DataError as error:
-            print(f'{error}: problems with processed data')
+            logging.error(error, 'problems with processed data')
         except InternalError as error:
-            print(f'{error}: database encounters an internal error')
+            logging.error(error, 'database encounters an internal error')
         except ProgrammingError as error:
-            print(f'{error}: wrong number of parameters')
+            logging.error(error, 'wrong number of parameters')
         except DatabaseError as error:
-            print(f'{error}: problems with database')
+            logging.error(error, 'problems with database')
         return False
 
     def add_deposit_info(self,
