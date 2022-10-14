8db9708270729249bbc7d439fb681ef79353f211
Michael <mihail.tchernov@yandex.ru>
2022-10-14 23:55:16 +0300

    Update docker client docstring

--- environment/environment/docker_client.py
+++ environment/environment/docker_client.py
@@ -1,5 +1,29 @@
-"""
-Модуль предназначен для работы с docker клиентом с помощью Docker SDK (Python).
+"""Module for working with docker client using with Docker SDK (Python).
+
+This module is useful working with docker client. You don't need use cmd line,
+only simple Python class.
+
+Examples:
+    Initialization::
+
+        from environment import CustomDockerClient
+        docker_client = CustomDockerClient()
+
+    Running container::
+
+        docker_client.create_image(
+            docker_root=dockerfile_root,
+            image_name=image_name
+        )
+
+        docker_client.run_container(
+            image_name=image_name,
+            environment_vars=['param1:val1', 'param2:val2'],
+            ports={'5432/tcp': 8133},
+            volumes=['storage/path:my/path'],
+            container_name=container_name
+        )
+
 """
 
 from typing import List, Dict, Set
@@ -10,30 +34,26 @@ from docker.models.containers import Container
 
 
 class CustomDockerClient:
-    """
-    Класс-адаптер для удобной работы с docker-клиентом.
-    """
+    """Class-adapter for simple working with docker client."""
     def __init__(self):
-        """
-        Конструктор класса. Здесь происходит сохранение docker-клиента.
-        """
+        """Initial class method."""
         self.__client = docker.from_env()
 
     @property
     def client(self) -> DockerClient:
-        """
-        Свойство возвращает адаптер подключения к docker.
+        """Return docker client object.
 
-        :return: docker-клиент
+        Returns: DockerClient object
+
         """
         return self.__client
 
     @property
     def images_tags(self) -> Set[str]:
-        """
-        Свойство возвращает все имеющиеся уникальные названия образов.
+        """Return all unique image names in docker.
 
-        :return: коллекция образов
+        Returns: collections of image names
+
         """
         image_names = set()
         for image in self.client.images.list(all=True):
@@ -41,56 +61,67 @@ class CustomDockerClient:
         return image_names
 
     def is_image_exist(self, image_name: str) -> bool:
-        """
-        Метод проверяет существование в системе образа с определенным именем.
+        """Return image existing by image name.
 
-        :param image_name: имя образа
-        :return: True - если образ найден, False - если образ отсутствует
+        Args:
+            image_name: target image name
+
+        Returns: True if image exists, False if image not found
+
         """
         return image_name in self.images_tags
 
     def remove_image(self, image_name: str):
-        """
-        Метод удаляет образ из системы по его имени.
+        """Remove image by image name.
 
-        :param image_name: имя образа
-        :return: None
+        Args:
+            image_name: target image name
+
+        Returns: None
+
         """
         self.client.images.remove(image=image_name)
 
     def create_image(self, docker_root: str, image_name: str):
-        """
-        Метод создает образ в системе из Docker файла с указанным именем.
+        """Create image from dockerfile with target name.
 
-        :param docker_root: папка, где расположен Docker файл
-        :param image_name: имя образа
-        :return: None
+        Args:
+            docker_root: root path with dockerfile
+            image_name: target image name
+
+        Returns: None
+
         """
         self.client.images.build(path=docker_root, tag=image_name)
 
     def run_container(self, image_name: str, environment_vars: List[str],
                       ports: Dict[str, int], volumes: List[str],
                       container_name: str):
-        """
-        Метод запускает контейнер из образа, который уже есть в системе.
+        """Run container from image. Image is exists in docker system.
 
-        Примеры записи входящих аргументов:
-        environment_vars = ['POSTGRES_DBASE=my_dbase', 'POSTGRES_USER=mikko']
+        Notes:
+            Examples of input arguments:
+            ::
 
-        ports = {'5432/tcp': 8133}.
-        Здесь:
-            - 5432 - порт внутри контейнера
-            - tcp - тип соединения контейнера с хостом
-            - 8133 - порт хоста
+                environment_vars = [
+                'POSTGRES_DBASE=my_dbase', 'POSTGRES_USER=mikko'
+                ]
+                ports = {'5432/tcp': 8133}
 
-        volumes = ['my/folder/path:/var/lib/postgresql/data']
+            Parameters for ports:
+                - 5432 - port INSIDE container
+                - tcp - connection type
+                - 8133 - host port
 
-        :param image_name: имя образа
-        :param environment_vars: список переменных окружения
-        :param ports: словарь портов
-        :param volumes: список точек монтирования файловых систем
-        :param container_name: имя создаваемого контейнера
-        :return: None
+        Args:
+            image_name: target image name
+            environment_vars: list of environment variables
+            ports: dict of ports
+            volumes: list of volumes
+            container_name: target container name
+
+        Returns: None
+
         """
         self.client.containers.run(
             image=image_name, environment=environment_vars, ports=ports,
@@ -98,27 +129,32 @@ class CustomDockerClient:
         )
 
     def get_container_by_name(self, name: str) -> Container:
-        """
-        Метод возвращает объект контейнера по его имени.
+        """Return container object by container name.
 
-        :param name: имя контейнера
-        :return: Container
+        Args:
+            name: target container name
+
+        Returns: Container object
+
         """
         return self.client.containers.get(container_id=name)
 
     def clear_system(self):
-        """
-        Метод удаляет неиспользуемые контейнеры.
+        """Clear docker from unused inactive containers.
 
-        :return: None
+        Returns: None
+
         """
         self.client.containers.prune()
 
     def remove_container(self, container_name: str):
-        """
-        Метод полностью удаляет контейнер из docker по его имени.
-        :param container_name: имя контейнера
-        :return: None
+        """Remove container from docker with image by container name.
+
+        Args:
+            container_name: target container name
+
+        Returns: None
+
         """
         container = self.get_container_by_name(name=container_name)
         container.stop()
