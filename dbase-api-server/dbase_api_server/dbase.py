c40c12407cb6b1a79dfb9d7104b798d0da766e18
Michael <mihail.tchernov@yandex.ru>
2022-10-15 11:53:05 +0300

    Add docstring to storage dbase module

--- dbase-api-server/dbase_api_server/dbase.py
+++ dbase-api-server/dbase_api_server/dbase.py
@@ -1,11 +1,32 @@
+"""Module for working with microservice Postgres database.
+
+The module contains wrapper class around Postgres database for saving
+storage metadata.
+
+Examples:
+    Adapter initialization::
+        from dbase_api_server.containers import PostgresConnectionParams
+        from environment import StorageDBase
+
+        connection_params = PostgresConnectionParams(
+            host='127.0.0.1',
+            port=5432,
+            user='user-1',
+            password='qwerty',
+            database='simple-dbase'
+        )
+        my_adapter = StorageDBase(params=connection_params)
+
+"""
+
 import logging
 
-from psycopg2._psycopg import connection
-from psycopg2 import connect
+from psycopg2._psycopg import connection as postgres_connection
+from psycopg2 import connect as connect_to_db
 from psycopg2 import OperationalError, DataError, InternalError
 from psycopg2 import ProgrammingError, DatabaseError
 
-from dbase_api_server.containers import ConnectionParams
+from dbase_api_server.containers import PostgresConnectionParams
 
 
 DEFAULT_PORT = 5432
@@ -13,8 +34,15 @@ DEFAULT_PATH = '/var/lib/postgresql/data'
 
 
 class StorageDBase:
-    def __init__(self, params: ConnectionParams):
-        self.__connection = connect(
+    """Database adapter for saving storage metadata."""
+
+    def __init__(self, params: PostgresConnectionParams):
+        """Initialize class method.
+
+        Args:
+            params: container with connection parameters (host, port, etc.)
+        """
+        self.__connection = connect_to_db(
             host=params.host,
             port=params.port,
             user=params.user,
@@ -24,11 +52,22 @@ class StorageDBase:
         self.cursor = self.__connection.cursor()
 
     @property
-    def connection(self) -> connection:
+    def connection(self) -> postgres_connection:
+        """Return postgres connection object.
+
+        Returns: connection object from psycopg2
+
+        """
         return self.__connection
 
     def __commit(self) -> bool:
-        """Save changes ahter query and show errors"""
+        """Commit changes in database.
+
+        If commit unsuccessful - create message in logger
+
+        Returns: True if commit is success, False - if commit  has some error.
+
+        """
         try:
             self.__connection.commit()
             return True
@@ -44,9 +83,15 @@ class StorageDBase:
             logging.error(error, 'problems with database')
         return False
 
-    def add_deposit_info(self,
-                         area_name: str) -> bool:
-        """Insert deposit data in 'deposits' table."""
+    def add_deposit_info(self, area_name: str) -> bool:
+        """Add deposit info to database.
+
+        Args:
+            area_name: deposit area name
+
+        Returns: True if info was added success, False - if not.
+
+        """
         insert_query = """
             INSERT INTO deposits(area_name)
             VALUES(%s);
