2ebdd87b3744c3ab147ba98d37847230fdaf136d
Michael <mihail.tchernov@yandex.ru>
2022-10-15 11:25:22 +0300

    Add docstrings to containers module

--- dbase-api-server/dbase_api_server/containers.py
+++ dbase-api-server/dbase_api_server/containers.py
@@ -1,9 +1,29 @@
+"""Module with common containers (dataclasses).
+
+This module organize information about objects.
+
+"""
+
 from dataclasses import dataclass
 from typing import List
 
 
+__all__ = ['PostgresConnectionParams']
+
+
 @dataclass
-class ConnectionParams:
+class PostgresConnectionParams:
+    """Container with connection parameters for Postgres access.
+
+    Args:
+        host: host address
+        port: port number
+        user: username in Postgres
+        password: user password
+        database: Postgres database name
+
+    """
+
     host: str
     port: int
     user: str
@@ -12,6 +32,11 @@ class ConnectionParams:
 
     @property
     def docker_env(self) -> List[str]:
+        """Return list object with environment variables for docker client.
+
+        Returns: list of environment variables
+
+        """
         return [
             f'POSTGRES_USER={self.user}',
             f'POSTGRES_PASSWORD={self.password}',
