91a5e0a1b6228d9ad7aae06434826c18ccce159d
Roostique <mannapovr@sigma-geophys.com>
2022-10-24 16:20:44 +0300

    Added get all deposits API method

--- dbase-api-server/dbase_api_server/main.py
+++ dbase-api-server/dbase_api_server/main.py
@@ -1,1 +1,32 @@
 """Microservice main module."""
+
+import os
+
+import dotenv
+from fastapi import FastAPI
+
+from dbase_api_server.containers import PostgresConnectionParams
+from dbase_api_server.dbase import StorageDBase
+
+dotenv.load_dotenv()
+
+
+app = FastAPI()
+dbase_adapter = StorageDBase(
+    PostgresConnectionParams(
+        user=os.getenv('POSTGRES_USER'),
+        password=os.getenv('POSTGRES_PASSWORD'),
+        host=os.getenv('POSTGRES_HOST'),
+        port=int(os.getenv('POSTGRES_PORT')),
+        dbname=os.getenv('POSTGRES_DBASE')
+    )
+)
+
+
+@app.get('/get-all-deposits')
+def get_all_deposits() -> bool:
+    """Return all deposits name from deposit table.
+
+    Returns: True if query completed success, False - if not.
+    """
+    return dbase_adapter.get_all_deposit_names()
