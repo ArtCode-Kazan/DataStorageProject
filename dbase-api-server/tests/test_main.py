8cdbe598c1620bba4fdf121e49f27929f782e61f
Roostique <mannapovr@sigma-geophys.com>
2022-10-28 14:05:48 +0300

    Fixed post requests according to review

--- dbase-api-server/tests/test_main.py
+++ dbase-api-server/tests/test_main.py
@@ -6,12 +6,12 @@ from hamcrest import assert_that, equal_to
 
 load_dotenv()
 
-app_host = os.getenv('APP_HOST')
-app_port = os.getenv('APP_PORT')
+APP_HOST = os.getenv('APP_HOST')
+APP_PORT = os.getenv('APP_PORT')
 
 
-def test_get_all_deposists(up_test_dbase, clear_deposits_table):
-    url = f'http://{app_host}:{app_port}/get-all-deposits'
+def test_get_all_deposits(up_test_dbase, clear_deposits_table):
+    url = f'http://{APP_HOST}:{APP_PORT}/get-all-deposits'
     response = requests.get(url)
     expected_value = {
         'status': True,
@@ -23,6 +23,10 @@ def test_get_all_deposists(up_test_dbase, clear_deposits_table):
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
+    )
+    assert_that(
+        actual_or_assertion=response.status_code,
+        matcher=equal_to(200)
     )
 
     area_name = 'test-area'
@@ -48,27 +52,44 @@ def test_get_all_deposists(up_test_dbase, clear_deposits_table):
 
 
 def test_add_new_deposit_name(clear_deposits_table):
-    area_name = 'test-name'
+    # area_name = 'test-name'
+    # expected_value = {
+    #     'status': True,
+    #     'message': f'Deposit name "{area_name}" added successfully',
+    #     'data': {}
+    # }
+    # url = f'http://{APP_HOST}:{APP_PORT}/add-deposit?area_name={area_name}'
+    # response = requests.post(url)
+    # assert_that(
+    #     actual_or_assertion=response.json(),
+    #     matcher=equal_to(expected_value)
+    # )
+    payload = {'area_name': 'test-name'}
+    url = f'http://{APP_HOST}:{APP_PORT}/add-deposit'
+    response = requests.post(url, params=payload)
     expected_value = {
         'status': True,
-        'message': f'Deposit name "{area_name}" added successfully',
+        'message': f'Deposit name "{payload["area_name"]}" added successfully',
         'data': {}
     }
-    url = f'http://{app_host}:{app_port}/add-deposit?area_name={area_name}'
-    response = requests.post(url)
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
     )
 
-    area_name = ''
+    assert_that(
+        actual_or_assertion=response.status_code,
+        matcher=equal_to(200)
+    )
+
+    payload = {'area_name': ''}
     expected_value = {
         'status': False,
-        'message': f'Cant add deposit name "{area_name}"',
+        'message': f'Cant add deposit name "{payload["area_name"]}"',
         'data': {}
     }
-    url = f'http://{app_host}:{app_port}/add-deposit?area_name={area_name}'
-    response = requests.post(url)
+    url = f'http://{APP_HOST}:{APP_PORT}/add-deposit'
+    response = requests.post(url, params=payload)
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
@@ -83,31 +104,39 @@ def test_update_deposit_name(up_test_dbase, clear_deposits_table):
     old_area_name = 'test-name'
     up_test_dbase.add_deposit_info(area_name=old_area_name)
 
-    new_area_name = 'test-name-2'
+    payload = {
+        'old_area_name': 'test-name',
+        'new_area_name': 'test-name-2'
+    }
     expected_value = {
         'status': True,
-        'message': (f'Deposit "{old_area_name}" successfully '
-                    f'renamed to "{new_area_name}"'),
+        'message': (f'Deposit "{payload["old_area_name"]}" successfully '
+                    f'renamed to "{payload["new_area_name"]}"'),
         'data': {}
     }
-    url = (f'http://{app_host}:{app_port}/update-deposit?'
-           f'old_area_name={old_area_name}&new_area_name={new_area_name}')
-    response = requests.post(url)
+    url = (f'http://{APP_HOST}:{APP_PORT}/update-deposit')
+    response = requests.post(url, params=payload)
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
     )
+    assert_that(
+        actual_or_assertion=response.status_code,
+        matcher=equal_to(200)
+    )
 
-    old_area_name = 'test-name-2'
-    new_area_name = ''
+    payload = {
+        'old_area_name': 'test-name-2',
+        'new_area_name': ''
+    }
     expected_value = {
         'status': False,
-        'message': f'Cant rename "{old_area_name} to "{new_area_name}"',
+        'message': (f'Cant rename "{payload["old_area_name"]} '
+                    f'to "{payload["new_area_name"]}"'),
         'data': {}
     }
-    url = (f'http://{app_host}:{app_port}/update-deposit?'
-           f'old_area_name={old_area_name}&new_area_name={new_area_name}')
-    response = requests.post(url)
+    url = (f'http://{APP_HOST}:{APP_PORT}/update-deposit')
+    response = requests.post(url, params=payload)
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
