8daa4d981f86a6a6f380c3ba752c879ccc39edf3
Roostique <mannapovr@sigma-geophys.com>
2022-10-28 10:35:49 +0300

    Added test add new depositn name

--- dbase-api-server/tests/test_main.py
+++ dbase-api-server/tests/test_main.py
@@ -1,9 +1,17 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 from hamcrest import assert_that, equal_to
+
+load_dotenv()
+
+app_host = os.getenv('APP_HOST')
+app_port = os.getenv('APP_PORT')
 
 
 def test_get_all_deposists(up_test_dbase, clear_deposits_table):
-    url = 'http://127.0.0.1:5435/get-all-deposits'
+    url = f'http://{app_host}:{app_port}/get-all-deposits'
     response = requests.get(url)
     expected_value = {
         'status': True,
@@ -29,6 +37,38 @@ def test_get_all_deposists(up_test_dbase, clear_deposits_table):
         }
     }
 
+    assert_that(
+        actual_or_assertion=response.json(),
+        matcher=equal_to(expected_value)
+    )
+    assert_that(
+        actual_or_assertion=response.status_code,
+        matcher=equal_to(200)
+    )
+
+
+def test_add_new_deposit_name(clear_deposits_table):
+    area_name = 'test-name'
+    expected_value = {
+        'status': True,
+        'message': f'Deposit name "{area_name}" added successfully',
+        'data': {}
+    }
+    url = f'http://{app_host}:{app_port}/add-deposit?area_name={area_name}'
+    response = requests.post(url)
+    assert_that(
+        actual_or_assertion=response.json(),
+        matcher=equal_to(expected_value)
+    )
+
+    area_name = ''
+    expected_value = {
+        'status': False,
+        'message': f'Cant add deposit name "{area_name}"',
+        'data': {}
+    }
+    url = f'http://{app_host}:{app_port}/add-deposit?area_name={area_name}'
+    response = requests.post(url)
     assert_that(
         actual_or_assertion=response.json(),
         matcher=equal_to(expected_value)
