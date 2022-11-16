72116ed59325148055bb91f66ece3c460d458209
Roostique <mannapovr@sigma-geophys.com>
2022-11-16 14:09:35 +0300

    Created tests update duplicate, lower and upper and update work fields name lengh

--- dbase-api-server/tests/test_dbase.py
+++ dbase-api-server/tests/test_dbase.py
@@ -581,3 +581,133 @@ class TestStorageDBase:
         cursor.execute(query)
         records_count = cursor.fetchone()[0]
         assert_that(actual_or_assertion=records_count, matcher=equal_to(1))
+
+    def test_update_duplicate_lower_upper_work(self, up_test_dbase,
+                                               clear_deposits_table):
+        up_test_dbase.add_deposit_info('test-area')
+
+        table = Table('deposits')
+        query = str(
+            Query.from_(table).select('id').where(
+                table.area_name == 'test-area'
+            )
+        )
+        cursor = up_test_dbase.connection.cursor()
+        cursor.execute(query)
+        area_id = cursor.fetchone()[0]
+
+        old_well_name = 'test-name'
+        old_start_time = '2022-11-15 12:12:12'
+        old_work_type = 'test-work'
+        old_deposit_id = area_id
+
+        old_work_info = WorkInfo(well_name=old_well_name,
+                                 start_time=old_start_time,
+                                 work_type=old_work_type,
+                                 deposit_id=old_deposit_id)
+        up_test_dbase.add_works_info(old_work_info)
+
+        new_well_name = 'TEST-NAME'
+        new_start_time = '2022-11-15 12:12:12'
+        new_work_type = 'TEST-WORK'
+        new_deposit_id = area_id
+
+        new_work_info = WorkInfo(well_name=new_well_name,
+                                 start_time=new_start_time,
+                                 work_type=new_work_type,
+                                 deposit_id=new_deposit_id)
+        is_succes = up_test_dbase.update_works_info(
+            old_work_info=old_work_info,
+            new_work_info=new_work_info
+        )
+        assert_that(
+            actual_or_assertion=is_succes,
+            matcher=is_(True)
+        )
+
+        table = Table('works')
+        query = str(
+            Query.from_(table).select(Count(1)).where(
+                table.well_name == new_well_name.lower()).where(
+                table.start_time == new_start_time).where(
+                table.work_type == new_work_type.lower()). where(
+                table.deposit_id == new_deposit_id
+            )
+        )
+        cursor = up_test_dbase.connection.cursor()
+        cursor.execute(query)
+        records_count = cursor.fetchone()[0]
+        assert_that(actual_or_assertion=records_count, matcher=equal_to(1))
+
+    @pytest.mark.parametrize(
+        ['passed_well_name',
+         'passed_work_type',
+         'expected_value',
+         'expected_records_count'],
+        [('a', 'b', True, 1),
+         ('a' * 10, 'b' * 20, True, 1),
+         ('a' * 11, 'b' * 22, False, 0)]
+    )
+    def test_update_work_fields_name_lengh(self, up_test_dbase,
+                                           clear_deposits_table,
+                                           passed_well_name,
+                                           passed_work_type,
+                                           expected_value,
+                                           expected_records_count):
+        up_test_dbase.add_deposit_info('test-area')
+
+        table = Table('deposits')
+        query = str(
+            Query.from_(table).select('id').where(
+                table.area_name == 'test-area'
+            )
+        )
+        cursor = up_test_dbase.connection.cursor()
+        cursor.execute(query)
+        area_id = cursor.fetchone()[0]
+
+        old_well_name = 'test-name'
+        old_start_time = '2022-11-15 12:12:12'
+        old_work_type = 'test-work'
+        old_deposit_id = area_id
+
+        old_work_info = WorkInfo(well_name=old_well_name,
+                                 start_time=old_start_time,
+                                 work_type=old_work_type,
+                                 deposit_id=old_deposit_id)
+        up_test_dbase.add_works_info(old_work_info)
+
+        new_well_name = passed_well_name
+        new_start_time = '2022-11-15 12:12:12'
+        new_work_type = passed_work_type
+        new_deposit_id = area_id
+
+        new_work_info = WorkInfo(well_name=new_well_name,
+                                 start_time=new_start_time,
+                                 work_type=new_work_type,
+                                 deposit_id=new_deposit_id)
+        is_succes = up_test_dbase.update_works_info(
+            old_work_info=old_work_info,
+            new_work_info=new_work_info
+        )
+        assert_that(
+            actual_or_assertion=is_succes,
+            matcher=is_(expected_value)
+        )
+
+        table = Table('works')
+        query = str(
+            Query.from_(table).select(Count(1)).where(
+                table.well_name == new_well_name).where(
+                table.start_time == new_start_time).where(
+                table.work_type == new_work_type). where(
+                table.deposit_id == new_deposit_id
+            )
+        )
+        cursor = up_test_dbase.connection.cursor()
+        cursor.execute(query)
+        records_count = cursor.fetchone()[0]
+        assert_that(
+            actual_or_assertion=records_count,
+            matcher=equal_to(expected_records_count)
+        )
