bb75b1efa5a6ab09b2fb5d51cbaf400f99d73254
Roostique <mannapovr@sigma-geophys.com>
2022-12-01 10:50:28 +0300

    Fixed expected value to expected response in test main

--- dbase-api-server/tests/test_main.py
+++ dbase-api-server/tests/test_main.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 from hamcrest import assert_that, equal_to
 from pypika import Query, Table
 
-from dbase_api_server.models import (Deposit, SeismicRecordInfo, StationInfo,
-                                     WorkInfo)
+from dbase_api_server.models import (Deposit, Response, SeismicRecordInfo,
+                                     StationInfo, WorkInfo)
 
 load_dotenv()
 
@@ -38,17 +38,17 @@ def test_get_all_deposits(up_test_dbase, clear_deposits_table):
     up_test_dbase.add_deposit_info(area_name=area_name)
 
     response = requests.get(url)
-    expected_value = {
-        'status': True,
-        'message': 'All deposits name returns successfully',
-        'data': {
+    expected_response = Response(
+        status=True,
+        message='All deposits name returns successfully',
+        data={
             'area_names': [area_name]
         }
-    }
+    )
 
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -76,16 +76,16 @@ def test_add_new_deposit(clear_deposits_table):
     )
 
     payload = Deposit(area_name='')
-    expected_value = {
-        'status': False,
-        'message': f'Cant add deposit name "{payload.area_name}"',
-        'data': {}
-    }
+    expected_response = Response(
+        status=False,
+        message=f'Cant add deposit name "{payload.area_name}"',
+        data={}
+    )
     url = f'{URL}/add-deposit'
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -109,18 +109,18 @@ def test_update_deposit(up_test_dbase, clear_deposits_table):
         'new_deposit': new_deposit.dict()
     }
 
-    expected_value = {
-        'status': True,
-        'message': (f'Deposit "{old_area_name}" successfully '
-                    f'renamed to "{new_area_name}"'),
-        'data': {}
-    }
+    expected_response = Response(
+        status=True,
+        message=(f'Deposit "{old_area_name}" successfully '
+                 f'renamed to "{new_area_name}"'),
+        data={}
+    )
     url = f'{URL}/update-deposit'
     response = requests.post(url, json=payload)
 
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -144,18 +144,18 @@ def test_update_blank_deposit_name(up_test_dbase, clear_deposits_table):
         'new_deposit': new_deposit.dict()
     }
 
-    expected_value = {
-        'status': False,
-        'message': (f'Cant rename "{old_area_name} '
-                    f'to "{new_area_name}"'),
-        'data': {}
-    }
+    expected_response = Response(
+        status=False,
+        message=(f'Cant rename "{old_area_name} '
+                 f'to "{new_area_name}"'),
+        data={}
+    )
     url = f'{URL}/update-deposit'
     response = requests.post(url, json=payload)
 
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response)
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -183,17 +183,17 @@ def test_add_new_work(up_test_dbase, clear_deposits_table):
         work_type='test-work',
         deposit_id=deposit_id
     )
-    expected_value = {
-        'status': True,
-        'message': (f'Successfully added work info: {payload.well_name}, '
-                    f'{payload.datetime_start_str}, {payload.work_type}, '
-                    f'{payload.deposit_id}'),
-        'data': {}
-    }
+    expected_response = Response(
+        status=True,
+        message=(f'Successfully added work info: {payload.well_name}, '
+                 f'{payload.datetime_start_str}, {payload.work_type}, '
+                 f'{payload.deposit_id}'),
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -223,17 +223,17 @@ def test_add_duplicate_work(up_test_dbase, clear_deposits_table):
     )
     requests.post(url, json=payload.dict())
 
-    expected_value = {
-        'status': False,
-        'message': (f'Cant add work info: {payload.well_name}, '
-                    f'{payload.datetime_start_str}, {payload.work_type}, '
-                    f'{payload.deposit_id}'),
-        'data': {}
-    }
+    expected_response = Response(
+        status=False,
+        message=(f'Cant add work info: {payload.well_name}, '
+                 f'{payload.datetime_start_str}, {payload.work_type}, '
+                 f'{payload.deposit_id}'),
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -271,9 +271,9 @@ def test_update_work_info(up_test_dbase, clear_deposits_table):
         'old_work_info': old_work_info.dict(),
         'new_work_info': new_work_info.dict()
     }
-    excepted_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Work info "{old_work_info.well_name}", '
             f'"{old_work_info.datetime_start_str}", '
             f'"{old_work_info.work_type}","{old_work_info.deposit_id}" '
@@ -281,13 +281,13 @@ def test_update_work_info(up_test_dbase, clear_deposits_table):
             f'"{new_work_info.datetime_start_str}", '
             f'"{new_work_info.work_type}", "{new_work_info.deposit_id}".'
         ),
-        'data': {}
-    }
+        data={}
+    )
     url = f'{URL}/update-work-info'
     response = requests.post(url, json=payload)
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(excepted_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -334,9 +334,9 @@ def test_update_duplicate_work_info(up_test_dbase,
         'old_work_info': old_work_info.dict(),
         'new_work_info': new_work_info.dict()
     }
-    excepted_value = {
-        'status': False,
-        'message': (
+    expected_response = Response(
+        status=False,
+        message=(
             f'Cant change "{old_work_info.well_name}", '
             f'"{old_work_info.datetime_start_str}", '
             f'"{old_work_info.work_type}","{old_work_info.deposit_id}" '
@@ -344,13 +344,13 @@ def test_update_duplicate_work_info(up_test_dbase,
             f'"{new_work_info.datetime_start_str}", '
             f'"{new_work_info.work_type}", "{new_work_info.deposit_id}".'
         ),
-        'data': {}
-    }
+        data={}
+    )
     url = f'{URL}/update-work-info'
     response = requests.post(url, json=payload)
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(excepted_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -390,21 +390,21 @@ def test_get_works_info(up_test_dbase, clear_deposits_table):
     url = f'{URL}/get-works-info/{deposit_id}'
     response = requests.get(url)
 
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'All works related to deposit with id:'
             f'{deposit_id} returend successfully'
         ),
-        'data': {
+        data={
             'works_info': [
                 first_work_info, second_work_info
             ]
         }
-    }
+    )
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -453,19 +453,19 @@ def test_add_station_info(up_test_dbase, clear_deposits_table):
         work_id=work_id_value
     )
     url = f'{URL}/add-station-info'
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Successfully added station info: {payload.station_number}, '
             f'{payload.x_wgs84}, {payload.y_wgs84}, '
             f'{payload.altitude}, {payload.work_id}'
         ),
-        'data': {}
-    }
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -516,19 +516,19 @@ def test_add_duplicate_station_info(up_test_dbase, clear_deposits_table):
     url = f'{URL}/add-station-info'
     requests.post(url, json=payload.dict())
 
-    expected_value = {
-        'status': False,
-        'message': (
+    expected_response = Response(
+        status=False,
+        message=(
             f'Cant add station info: {payload.station_number}, '
             f'{payload.x_wgs84}, {payload.y_wgs84}, {payload.altitude}, '
             f'{payload.work_id}'
         ),
-        'data': {}
-    }
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -586,9 +586,9 @@ def test_update_station_info(up_test_dbase, clear_deposits_table):
         altitude=77.333333,
         work_id=work_id_value
     )
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Successfully changed station info: '
             f'{old_station_info.station_number}, {old_station_info.x_wgs84}, '
             f'{old_station_info.y_wgs84}, {old_station_info.altitude}, '
@@ -597,8 +597,8 @@ def test_update_station_info(up_test_dbase, clear_deposits_table):
             f'{new_station_info.x_wgs84}, {new_station_info.y_wgs84},'
             f' {new_station_info.altitude}, {new_station_info.work_id}.'
         ),
-        'data': {}
-    }
+        data={}
+    )
     payload = {
         'old_station_info': old_station_info.dict(),
         'new_station_info': new_station_info.dict()
@@ -607,7 +607,7 @@ def test_update_station_info(up_test_dbase, clear_deposits_table):
     response = requests.post(url, json=payload)
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -675,9 +675,9 @@ def test_update_duplicate_station_info(up_test_dbase, clear_deposits_table):
         altitude=33.3,
         work_id=work_id_value
     )
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Successfully changed station info: '
             f'{old_station_info.station_number}, {old_station_info.x_wgs84}, '
             f'{old_station_info.y_wgs84}, {old_station_info.altitude},'
@@ -686,8 +686,8 @@ def test_update_duplicate_station_info(up_test_dbase, clear_deposits_table):
             f'{new_station_info.x_wgs84}, {new_station_info.y_wgs84}, '
             f'{new_station_info.altitude}, {new_station_info.work_id}.'
         ),
-        'data': {}
-    }
+        data={}
+    )
     payload = {
         'old_station_info': old_station_info.dict(),
         'new_station_info': new_station_info.dict()
@@ -696,7 +696,7 @@ def test_update_duplicate_station_info(up_test_dbase, clear_deposits_table):
     response = requests.post(url, json=payload)
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -758,22 +758,22 @@ def test_get_stations_info(up_test_dbase, clear_deposits_table):
     url = f'{URL}/get-stations-info/{work_id_value}'
     response = requests.get(url)
 
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'All stations related to work with id:"{work_id_value}" '
             f'returend successfully'
         ),
-        'data': {
+        data={
             'stations_info': [
                 first_station_info.dict(),
                 second_station_info.dict()
             ]
         }
-    }
+    )
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -846,9 +846,9 @@ def test_add_seismic_record_info(up_test_dbase, clear_deposits_table):
         is_using=True
     )
     url = f'{URL}/add-seimic-record-info'
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Successfully added seismic record info: '
             f'{payload.station_id}, '
             f'{payload.datetime_start_str}, '
@@ -856,12 +856,12 @@ def test_add_seismic_record_info(up_test_dbase, clear_deposits_table):
             f'{payload.frequency}, {payload.origin_name}, '
             f'{payload.unique_name}, {payload.is_using}'
         ),
-        'data': {}
-    }
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -948,9 +948,9 @@ def test_update_duplicte_seismic_record_info(
         is_using=True
     )
     url = f'{URL}/add-seimic-record-info'
-    expected_value = {
-        'status': False,
-        'message': (
+    expected_response = Response(
+        status=False,
+        message=(
             f'Cant add seismic record info: '
             f'{payload.station_id}, '
             f'{payload.datetime_start_str}, '
@@ -958,12 +958,12 @@ def test_update_duplicte_seismic_record_info(
             f'{payload.frequency}, {payload.origin_name}, '
             f'{payload.unique_name}, {payload.is_using}'
         ),
-        'data': {}
-    }
+        data={}
+    )
     response = requests.post(url, json=payload.dict())
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -1047,9 +1047,9 @@ def test_update_seismic_records_info(up_test_dbase, clear_deposits_table):
         unique_name='test-name-4',
         is_using=True
     )
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'Successfully changed seismic record info '
             f'{old_record_info.station_id}, '
             f'{old_record_info.datetime_start_str}, '
@@ -1062,8 +1062,8 @@ def test_update_seismic_records_info(up_test_dbase, clear_deposits_table):
             f'{new_record_info.frequency}, {new_record_info.origin_name}, '
             f'{new_record_info.unique_name}, {new_record_info.is_using}.'
         ),
-        'data': {}
-    }
+        data={}
+    )
     payload = {
         'old_record_info': old_record_info.dict(),
         'new_record_info': new_record_info.dict()
@@ -1072,7 +1072,7 @@ def test_update_seismic_records_info(up_test_dbase, clear_deposits_table):
     response = requests.post(url, json=payload)
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
@@ -1169,9 +1169,9 @@ def test_update_duplicate_seismic_records_info(up_test_dbase,
         unique_name='test-name-2',
         is_using=True
     )
-    expected_value = {
-        'status': False,
-        'message': (
+    expected_value = Response(
+        status=False,
+        message=(
             f'Cant change seismic record info '
             f'{old_record_info.station_id}, '
             f'{old_record_info.datetime_start_str}, '
@@ -1184,8 +1184,8 @@ def test_update_duplicate_seismic_records_info(up_test_dbase,
             f'{new_record_info.frequency}, {new_record_info.origin_name}, '
             f'{new_record_info.unique_name}, {new_record_info.is_using}.'
         ),
-        'data': {}
-    }
+        data={}
+    )
     payload = {
         'old_record_info': old_record_info.dict(),
         'new_record_info': new_record_info.dict()
@@ -1284,22 +1284,22 @@ def test_get_seismic_records_info(up_test_dbase, clear_deposits_table):
     url = f'{URL}/get-seismic-records-info/{station_id_value}'
     response = requests.get(url)
 
-    expected_value = {
-        'status': True,
-        'message': (
+    expected_response = Response(
+        status=True,
+        message=(
             f'All seismic records related to station '
             f'with id:"{station_id_value}" returend successfully'
         ),
-        'data': {
+        data={
             'records_info': [
                 record_info,
                 new_record_info
             ]
         }
-    }
+    )
     assert_that(
         actual_or_assertion=response.json(),
-        matcher=equal_to(expected_value)
+        matcher=equal_to(expected_response.dict())
     )
     assert_that(
         actual_or_assertion=response.status_code,
